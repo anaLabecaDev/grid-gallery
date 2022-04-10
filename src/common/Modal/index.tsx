7a2a1d15abb586be8b4a528ec95276d9c07dd16f
Ana Labeca <anafrost01@gmail.com>
2022-04-10 20:03:41 +0100

    feat: ✨ Create portal and modal component

--- src/common/Modal/index.tsx
+++ src/common/Modal/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import ReactPortal from '../ReactPortal';
+import styles from './modal.module.scss';
 
-function Modal() {
-  return <div>Modal</div>;
+interface IModalProps {
+  children: React.ReactNode;
+  onClose: () => void;
+  isOpen: boolean;
+}
+
+function Modal({ children, onClose, isOpen }: IModalProps) {
+  const onKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape' && isOpen) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      document.body.removeEventListener('keydown', onKeyDown);
+    };
+  }, [isOpen]);
+
+  return isOpen ? (
+    <ReactPortal wrapperId="modal-portal-container">
+      <div className={styles.modalOverlay}>{children}</div>
+    </ReactPortal>
+  ) : null;
 }
 
 export default Modal;
