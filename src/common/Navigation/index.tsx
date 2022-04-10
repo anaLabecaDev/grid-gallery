498b610e6ab4c62448cb5d70fc57d4964f1225fa
Ana Labeca <anafrost01@gmail.com>
2022-04-10 14:08:57 +0100

    feat: Navigation
    - Added svgs to the project
    
    - Add menu items and styled navigation

--- src/common/Navigation/index.tsx
+++ src/common/Navigation/index.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import { Link, LinkProps, NavLink } from 'react-router-dom';
 import styles from './navigation.module.scss';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { ReactComponent as Frame } from '../../assets/frame.svg';
+import { ReactComponent as Heart } from '../../assets/heart.svg';
+
+const menuItems = [
+  {
+    name: 'Home',
+    icon: <Frame title="list" />,
+    to: '/',
+  },
+  {
+    name: 'Favorites',
+    icon: <Heart title="favorites" />,
+    to: '/favorites',
+  },
+];
 
 function Navigation() {
-  return <nav className={styles.nav}>N</nav>;
+  return (
+    <nav className={styles.navbar}>
+      <Logo className={styles.logo} title="logo" />
+
+      <ul className={styles.menu}>
+        {menuItems.map((item) => (
+          <li className={styles.menuItem}>
+            <NavLink
+              key={item.name}
+              to={item.to}
+              className={({ isActive }) => (isActive ? styles.active : styles.inactive)}
+            >
+              {item.icon}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Navigation;
